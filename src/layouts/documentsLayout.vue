329a20aab1d773be8351c38b7924fd986ecb246a
Rasim Matsko <rasimmatsko0@gmail.com>
2024-03-19 15:33:29 +0200

    Edit 19.03 2

--- src/layouts/documentsLayout.vue
+++ src/layouts/documentsLayout.vue
@@ -130,7 +130,7 @@
                
                 <td class="px-6 py-4 text-right flex align-items-center justify-end">
 
-                    <a target="_blank" :href="document.filePath" type="button"
+                    <a target="_blank" :href="'http://149.100.159.188' + document.filePath" type="button"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 mr-3 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                     <svg width="24" height="18" fill="#fff" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm.002 3c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5z" fill-rule="nonzero"/></svg>
                 </a>
